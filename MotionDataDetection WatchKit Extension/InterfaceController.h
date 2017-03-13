66cb0a175e506c15e610f283d45f834dc947d9d1
qianyi wang <qianyiwang@qianyis-MacBook-Pro.local>
2017-03-12 21:27:02 -0400

    add calibration filter to acc

--- MotionDataDetection WatchKit Extension/InterfaceController.h
+++ MotionDataDetection WatchKit Extension/InterfaceController.h
@@ -15,9 +15,14 @@
 @property (strong, nonatomic) IBOutlet WKInterfaceLabel *acc_y_text;
 @property (strong, nonatomic) IBOutlet WKInterfaceLabel *acc_z_text;
 @property (nonatomic) double time_interval;
+@property (nonatomic) NSMutableArray *acc_array;
+@property (nonatomic) NSMutableArray *mAccBias;
+@property (nonatomic) NSMutableArray *mAccDelta;
 @property (nonatomic) NSMutableArray *speed_last;
 @property (nonatomic) NSMutableArray *mSpeedBias;
 @property (nonatomic) NSMutableArray *mSpeedDelta;
+@property (nonatomic) NSArray *ACC_BIAS_STEP;
+@property (nonatomic) NSArray *ACC_ZERO_RANGE;
 @property (nonatomic) NSArray *SPEED_BIAS_STEP;
 @property (nonatomic) NSArray *SPEED_ZERO_RANGE;
 
--- MotionDataDetection WatchKit Extension/InterfaceController.m
+++ MotionDataDetection WatchKit Extension/InterfaceController.m
@@ -21,12 +21,17 @@
 @synthesize acc_x_text;
 @synthesize acc_y_text;
 @synthesize acc_z_text;
+@synthesize acc_array;
 @synthesize speed_last;
 @synthesize time_interval;
+@synthesize mAccBias;
+@synthesize mAccDelta;
 @synthesize mSpeedBias;
 @synthesize mSpeedDelta;
 @synthesize SPEED_BIAS_STEP;
 @synthesize SPEED_ZERO_RANGE;
+@synthesize ACC_BIAS_STEP;
+@synthesize ACC_ZERO_RANGE;
 
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
@@ -39,6 +44,21 @@
     motionManager = [[CMMotionManager alloc] init];
     motionManager.deviceMotionUpdateInterval = time_interval;
     time_interval = 0.05;
+    
+    acc_array = [[NSMutableArray alloc] init];
+    [acc_array addObject:[NSNumber numberWithDouble:0]];
+    [acc_array addObject:[NSNumber numberWithDouble:0]];
+    [acc_array addObject:[NSNumber numberWithDouble:0]];
+    
+    mAccDelta = [[NSMutableArray alloc] init];
+    [mAccDelta addObject:[NSNumber numberWithDouble:0]];
+    [mAccDelta addObject:[NSNumber numberWithDouble:0]];
+    [mAccDelta addObject:[NSNumber numberWithDouble:0]];
+    
+    mAccBias = [[NSMutableArray alloc] init];
+    [mAccBias addObject:[NSNumber numberWithDouble:0]];
+    [mAccBias addObject:[NSNumber numberWithDouble:0]];
+    [mAccBias addObject:[NSNumber numberWithDouble:0]];
     
     speed_last = [[NSMutableArray alloc] init];
     [speed_last addObject:[NSNumber numberWithDouble:0]];
@@ -55,11 +75,18 @@
     [mSpeedBias addObject:[NSNumber numberWithDouble:0]];
     [mSpeedBias addObject:[NSNumber numberWithDouble:0]];
     
+    ACC_BIAS_STEP = [[NSArray alloc] init];
+    ACC_BIAS_STEP = @[@0.01, @0.01, @0.01];
+    
+    ACC_ZERO_RANGE= [[NSArray alloc] init];
+    ACC_ZERO_RANGE = @[@0.1, @0.1, @0.1];
+    
     SPEED_BIAS_STEP = [[NSArray alloc] init];
     SPEED_BIAS_STEP = @[@0.1, @0.1, @0.1];
     
     SPEED_ZERO_RANGE= [[NSArray alloc] init];
     SPEED_ZERO_RANGE = @[@0.05, @0.05, @0.05];
+    
 }
 
 - (void)willActivate {
@@ -74,11 +101,53 @@
 
 -(void)startSensorUpdate{
     [motionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMDeviceMotion  *deviceMotion, NSError *error) {
+        
+        // apply calibration filter for Acceleration
+        
+        [acc_array replaceObjectAtIndex:0 withObject:[NSNumber numberWithDouble:deviceMotion.userAcceleration.x]];
+        [acc_array replaceObjectAtIndex:1 withObject:[NSNumber numberWithDouble:deviceMotion.userAcceleration.y]];
+        [acc_array replaceObjectAtIndex:2 withObject:[NSNumber numberWithDouble:deviceMotion.userAcceleration.z]];
+        
+        for(int i=0; i<3; i++){
+
+            double temp = [acc_array[i] doubleValue] - [mAccBias[i] doubleValue];
+            [mAccDelta replaceObjectAtIndex:i withObject:[NSNumber numberWithDouble:temp]];
+            
+            if ([mAccDelta[i] doubleValue]> 0){
+                if ([mAccDelta[i] doubleValue] > [ACC_BIAS_STEP[i] doubleValue]) {
+                    [mAccBias replaceObjectAtIndex:i withObject:
+                     [NSNumber numberWithDouble:[mAccBias[i] doubleValue]+[ACC_BIAS_STEP[i] doubleValue]]];
+                } else {
+                    [mAccBias replaceObjectAtIndex:i withObject:[NSNumber numberWithDouble:
+                                                                   [mAccBias[i] doubleValue]+[acc_array[i] doubleValue]]];
+                }
+            }
+            else{
+                if ([mAccDelta[i] doubleValue] < -[ACC_BIAS_STEP[i] doubleValue]) {
+                    [mAccBias replaceObjectAtIndex:i withObject:
+                     [NSNumber numberWithDouble:[mAccBias[i] doubleValue]-[ACC_BIAS_STEP[i] doubleValue]]];
+                } else {
+                    [mAccBias replaceObjectAtIndex:i withObject:[NSNumber numberWithDouble:
+                                                                   [mAccBias[i] doubleValue]-[acc_array[i] doubleValue]]];
+                }
+            }
+            [mSpeedDelta replaceObjectAtIndex:i withObject:[NSNumber numberWithDouble:
+                                                            [acc_array[i] doubleValue]-[mAccBias[i] doubleValue]]];
+            
+            if([mAccDelta[i] doubleValue]<[ACC_ZERO_RANGE[i] doubleValue] && [mAccDelta[i] doubleValue]>-[ACC_ZERO_RANGE[i] doubleValue]){
+                [mAccDelta replaceObjectAtIndex:i withObject:[NSNumber numberWithDouble:0]];
+            }
+        }
+        // end calibration filter for Acceleration
+        
         CMAttitude *attitude = deviceMotion.attitude;
         CMRotationMatrix rot=attitude.rotationMatrix;
-        double acc_global_x = deviceMotion.userAcceleration.x*rot.m11 + deviceMotion.userAcceleration.y*rot.m21 + deviceMotion.userAcceleration.z*rot.m31;
-        double acc_global_y = deviceMotion.userAcceleration.x*rot.m12 + deviceMotion.userAcceleration.y*rot.m22 + deviceMotion.userAcceleration.z*rot.m32;
-        double acc_global_z = deviceMotion.userAcceleration.x*rot.m13 + deviceMotion.userAcceleration.y*rot.m23 + deviceMotion.userAcceleration.z*rot.m33;
+        double acc_global_x = [mAccDelta[0] doubleValue]*rot.m11 + [mAccDelta[1] doubleValue]*rot.m21 + [mAccDelta[2] doubleValue]*rot.m31;
+        double acc_global_y = [mAccDelta[0] doubleValue]*rot.m12 + [mAccDelta[1] doubleValue]*rot.m22 + [mAccDelta[2] doubleValue]*rot.m32;
+        double acc_global_z = [mAccDelta[0] doubleValue]*rot.m13 + [mAccDelta[1] doubleValue]*rot.m23 + [mAccDelta[2] doubleValue]*rot.m33;
+        
+        
+        NSLog(@"%f,%f,%f", acc_global_x, acc_global_y, acc_global_z);
         
         self.acc_x_text.text = [NSString stringWithFormat:@"%f", acc_global_x];
         self.acc_y_text.text = [NSString stringWithFormat:@"%f", acc_global_y];
@@ -88,9 +157,9 @@
         //        speed_last_y += acc_global_y*time_interval;
         //        speed_last_z += acc_global_z*time_interval;
         
-        [self calculateSpeed:acc_global_x accY:acc_global_y accZ:acc_global_z];
-        [self speedCalibrationFilter];
-        NSLog(@"acc_x: %f, acc_y: %f, acc_z: %f, speed_x: %@, speed_y: %@, speed_z: %@", acc_global_x, acc_global_y, acc_global_z, mSpeedDelta[0], mSpeedDelta[1], mSpeedDelta[2]);
+//        [self calculateSpeed:acc_global_x accY:acc_global_y accZ:acc_global_z];
+//        [self speedCalibrationFilter];
+//        NSLog(@"acc_x: %f, acc_y: %f, acc_z: %f, speed_x: %@, speed_y: %@, speed_z: %@", acc_global_x, acc_global_y, acc_global_z, mSpeedDelta[0], mSpeedDelta[1], mSpeedDelta[2]);
     }];
 }
 
@@ -143,10 +212,20 @@
 }
 
 - (IBAction)startButtonAction {
+    [[WKInterfaceDevice currentDevice] playHaptic:WKHapticTypeClick];
+//    dispatch_queue_t async_queue = dispatch_queue_create("async queue", DISPATCH_QUEUE_CONCURRENT);
+//    dispatch_async(async_queue, ^{
+//        [self startSensorUpdate];
+//    });
     [self startSensorUpdate];
 }
 - (IBAction)stopButtonAction {
-    motionManager.stopDeviceMotionUpdates;
+    [[WKInterfaceDevice currentDevice] playHaptic:WKHapticTypeClick];
+//    dispatch_async(dispatch_get_main_queue(), ^{
+//        NSLog(@"stop update");
+//        [motionManager stopDeviceMotionUpdates];
+//    });
+    [motionManager stopDeviceMotionUpdates];
     [self resetVariables];
 }
 
